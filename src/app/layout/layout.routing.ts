c6bc282b692699d1072a7ccebd7e8b50393e2926
juanbracamontes <juanbracamontes02@gmail.com>
2017-12-18 13:25:37 -0700

    Ruta usuarios eliminada

--- src/app/layout/layout.routing.ts
+++ src/app/layout/layout.routing.ts
@@ -20,9 +20,9 @@ const LAYOUT_ROUTES: Routes = [
     {
         path: '', component: LayoutComponent, children: [
         //Home
-        {path: '', redirectTo: 'usuarios', pathMatch: 'full'},
+        {path: '', redirectTo: 'categorias', pathMatch: 'full'},
 
-        {path: 'usuarios', loadChildren: '../pages/usuarios/usuarios.module#UsuariosModule'},
+        // {path: 'usuarios', loadChildren: '../pages/usuarios/usuarios.module#UsuariosModule'},
         {path: 'categorias', loadChildren: '../pages/categorias/categorias.module#CategoriasModule'},
         {
             path: 'perfil-ps/:serviceProviderKey',
